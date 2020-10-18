7f8cfce37274b1c749213fa23fe3dc40272b0256
Ivan Wilhelm <ivan.whm@icloud.com>
2020-10-18 10:28:14 -0300

    Getting Some Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {
+      title: 'Black Lives Matter',
+      url: 'https://is.gd/tnK31A',
+    },
+    {
+      title: 'At the F1',
+      url: 'https://is.gd/Q0S1aM',
+    },
+    {
+      title: 'At home',
+      url: 'https://is.gd/LYXAtR',
+    },
+    {
+      title: 'Working from home',
+      url: 'https://is.gd/OFeNAF',
+    },
+    {
+      title: 'Desert',
+      url: 'https://is.gd/WN1pI9',
+    },
+    {
+      title: 'Hong Kong City',
+      url: '',
+    },
+    {
+      title: 'At the beach',
+      url: 'https://is.gd/Ix5QJd',
+    },
+  ];
 }
