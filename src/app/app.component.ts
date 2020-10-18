d31425891811eb4c94c86959d47416eddf20ed84
Ivan Wilhelm <ivan.whm@icloud.com>
2020-10-18 11:10:40 -0300

    Reminder on NgIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,5 +36,61 @@ export class AppComponent {
       title: 'At the beach',
       url: 'https://is.gd/Ix5QJd',
     },
+    {
+      title: 'Black Lives Matter',
+      url: 'https://is.gd/tnK31A',
+    },
+    {
+      title: 'At the F1',
+      url: 'https://is.gd/Q0S1aM',
+    },
+    {
+      title: 'At home',
+      url: 'https://is.gd/LYXAtR',
+    },
+    {
+      title: 'Working from home',
+      url: 'https://is.gd/OFeNAF',
+    },
+    {
+      title: 'Desert',
+      url: 'https://is.gd/WN1pI9',
+    },
+    {
+      title: 'Hong Kong City',
+      url: 'https://is.gd/tSPlwG',
+    },
+    {
+      title: 'At the beach',
+      url: 'https://is.gd/Ix5QJd',
+    },
+    {
+      title: 'Black Lives Matter',
+      url: 'https://is.gd/tnK31A',
+    },
+    {
+      title: 'At the F1',
+      url: 'https://is.gd/Q0S1aM',
+    },
+    {
+      title: 'At home',
+      url: 'https://is.gd/LYXAtR',
+    },
+    {
+      title: 'Working from home',
+      url: 'https://is.gd/OFeNAF',
+    },
+    {
+      title: 'Desert',
+      url: 'https://is.gd/WN1pI9',
+    },
+    {
+      title: 'Hong Kong City',
+      url: 'https://is.gd/tSPlwG',
+    },
+    {
+      title: 'At the beach',
+      url: 'https://is.gd/Ix5QJd',
+    },
   ];
 }
